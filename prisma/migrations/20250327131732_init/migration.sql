507e6e2dd76f7350fec3997eb50db28e45aae6b5
brian ngeywo <brianngeywo254@gmail.com>
2025-03-27 19:18:09 +0300

    Day 3

--- prisma/migrations/20250327131732_init/migration.sql
+++ prisma/migrations/20250327131732_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "FoodPlace" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "description" TEXT,
+    "image" TEXT,
     "location" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
@@ -12,6 +13,7 @@ CREATE TABLE "FoodPlace" (
 CREATE TABLE "Menu" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
+    "image" TEXT,
     "foodPlaceId" INTEGER NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
@@ -23,6 +25,7 @@ CREATE TABLE "FoodItem" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "description" TEXT,
+    "image" TEXT,
     "price" REAL NOT NULL,
     "isAvailable" BOOLEAN NOT NULL DEFAULT true,
     "readyInMins" INTEGER,
